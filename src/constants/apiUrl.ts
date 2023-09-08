b3538de75e9b30667b35336d83d38efa8a40123a
brother1-4752 <guddlf4752@gmail.com>
2023-09-08 10:19:30 +0900

    chore: api와 cache관련 상수 생성

--- src/constants/apiUrl.ts
+++ src/constants/apiUrl.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'http://localhost:4000/sick';
+export const API_BASE_URL = 'http://localhost:4000';
+
+export const URL_SICK = '/sick';
---
+++ src/constants/cache.ts
@@ -0,0 +1,3 @@
+export const CACHE_KEY = 'MY_CACHE';
+
+export const CACHE_EXPIRE_TIME = 1000 * 60; // 1분
