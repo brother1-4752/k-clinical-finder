fcdfc42fb868f05539d844ebfafad0cc615e9735
brother1-4752 <guddlf4752@gmail.com>
2023-09-08 10:20:06 +0900

    chore: 디바운스 delay 값 수정 / 콘솔로그 삭제

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -5,18 +5,16 @@ const useDebounce = (keyword: string) => {
   const [debounceValue, setDebounceValue] = useState<string>('');
   let isKoreanWord = validateIsKoreanWord(keyword);
 
-  const delay = 200;
+  const delay = 300;
 
   useEffect(() => {
     if (isKoreanWord) {
       const timer = setTimeout(() => {
         setDebounceValue(keyword.trim());
-        console.log('디바운스 실행');
       }, delay);
       return () => clearTimeout(timer);
     }
     const timer = setTimeout(() => {
-      console.log('디바운스값 초기화');
       setDebounceValue('');
     }, delay);
     return () => clearTimeout(timer);
